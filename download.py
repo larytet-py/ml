562311fb39ead5062becaa6d55f7da5c40d71c45
arkady <>
2024-04-28 17:11:51 +0300

    insert data

--- download.py
+++ download.py
@@ -1,21 +1,22 @@
+import argparse
 import requests
 import zipfile
 import os
 import sys
 from datetime import datetime, timedelta
 import csv
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ProcessPoolExecutor
 from clickhouse_connect import get_client
 import logging
 
 # Setup the logging configuration
 logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def check_and_insert_data(csv_path, table_name):
+def check_and_insert_data(csv_name, table_name):
     client = get_client()
     column_names = ['id', 'price', 'qty', 'base_qty', 'time', 'is_buyer_maker', 'unknown_flag'] 
 
-    with open(csv_path, 'r') as file:
+    with open(csv_name, 'r') as file:
         reader = csv.reader(file)
         rows = list(reader)
         if not rows:
@@ -28,17 +29,15 @@ def check_and_insert_data(csv_path, table_name):
     count_result = result.result_rows[0][0] if result.result_rows else 0
 
     if count_result < 2:
-        logging.error(f"One or both timestamps ({first_timestamp}, {last_timestamp}) are missing in {table_name} for {csv_path}. Inserting data.")
-        with open(csv_path, 'r') as file:
+        logging.error(f"One or both timestamps ({first_timestamp}, {last_timestamp}) are missing in {table_name} for {csv_name}. Inserting data.")
+        with open(csv_name, 'r') as file:
             reader = csv.reader(file)
             data = [row for row in reader]
             if data:
-                logging.debug("Column names: %s", column_names)
-                logging.debug("First row of data: %s", data[0])
                 client.insert(table=table_name, data=data, column_names=column_names)
-        logging.info(f"Data from {csv_path} inserted.")
+        logging.info(f"Data from {csv_name} inserted.")
     else:
-        logging.info("Both timestamps are present. No insertion needed.")
+        logging.info(f"Both timestamps are present for {csv_name}. No insertion needed.")
 
 def download_and_unpack(url, file_name, csv_name, table_name):
     csv_path = f"./data/{csv_name}"
@@ -74,27 +73,33 @@ def download_files(symbol, start_date, end_date, num_workers, table_name):
         csv_name = f"{symbol}USDT-trades-{date_str}.csv"
         download_and_unpack(url, file_name, csv_name, table_name)
 
-if __name__ == "__main__":
-    symbol = "BTC"
-    start_date = datetime.strptime("2024-01-01", "%Y-%m-%d")
-    end_date = datetime.now()
-    num_workers = 1
-    table_name = 'trades'
+def download_files_process(symbol, start_date, end_date, num_workers, table_name):
+    base_url = "https://data.binance.vision/data/spot/daily/trades/{symbol}USDT/{symbol}USDT-trades-{date}.zip"
+    dates = [start_date + timedelta(days=x) for x in range((end_date - start_date).days + 1)]
 
-    args = sys.argv[1:]
-    if len(args) >= 1:
-        symbol = args[0]
-    if len(args) >= 3:
-        start_date = datetime.strptime(args[1], "%Y-%m-%d")
-        end_date = datetime.strptime(args[2], "%Y-%m-%d")
-    if len(args) >= 4:
-        num_workers = int(args[3])
-    if len(args) >= 5:
-        log_level = args[4].upper()
-        if log_level in ['DEBUG', 'ERROR', 'NONE']:
-            logging.getLogger().setLevel(getattr(logging, log_level))
+    with ProcessPoolExecutor(max_workers=num_workers) as executor:
+        futures = []
+        for current_date in dates:
+            date_str = current_date.strftime("%Y-%m-%d")
+            url = base_url.format(symbol=symbol, date=date_str)
+            file_name = f"{symbol}USDT-trades-{date_str}.zip"
+            csv_name = f"{symbol}USDT-trades-{date_str}.csv"
+            futures.append(executor.submit(download_and_unpack, url, file_name, csv_name, table_name))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download and process trade data files.")
+    parser.add_argument('--symbol', type=str, default='BTC', help='Symbol to process, e.g., BTC')
+    parser.add_argument('--start_date', default=datetime.strptime("2024-01-01", "%Y-%m-%d"), type=lambda s: datetime.strptime(s, "%Y-%m-%d"), help='Start date in YYYY-MM-DD format')
+    parser.add_argument('--end_date', default=datetime.now(), type=lambda s: datetime.strptime(s, "%Y-%m-%d"), help='End date in YYYY-MM-DD format')
+    parser.add_argument('--num_workers', type=int, default=2, help='Number of worker processes/threads')
+    parser.add_argument('--table_name', type=str, default='trades', help='Name of the database table to insert data into')
+    parser.add_argument('--log_level', type=str, choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='ERROR', help='Set the logging level')
+
+    args = parser.parse_args()
+
+    logging.getLogger().setLevel(args.log_level.upper())
 
     if not os.path.exists("./data/"):
         os.makedirs("./data/")
 
-    download_files(symbol, start_date, end_date, num_workers, table_name)
+    download_files_process(symbol=args.symbol, start_date=args.start_date, end_date=args.end_date, num_workers=args.num_workers, table_name=args.table_name)
