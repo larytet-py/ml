7ec0c996181a29f93ccf6418c735868b40d3c6f8
arkady <>
2024-05-18 08:47:10 +0300

    trades density timestamps

--- tradesdensity.py
+++ tradesdensity.py
@@ -50,10 +50,11 @@ def process_data_in_chunks(query, chunk_size, interval):
             max_density_record = max(chunk_trade_density, key=lambda x: x[0])
             min_density, min_density_time, min_open_price, min_close_price = min_density_record
             max_density, max_density_time, max_open_price, max_close_price = max_density_record
+            min_log_density, max_log_density = math.log(min_density),math.log(max_density)
             chunk_timestamp = chunk_df['timestamp'].iloc[0]
             logger.debug(f"{chunk_timestamp}: "
-                         f"{min_density}@{min_density_time}, price={min_open_price}/{min_close_price},"
-                         f"{max_density}@{max_density_time}, price={max_open_price}/{max_close_price}"
+                         f"{min_log_density:.2f}@{min_density_time}, price={min_open_price:.0f}/{min_close_price:.0f},"
+                         f"{max_log_density:.2f}@{max_density_time}, price={max_open_price:.0f}/{max_close_price:.0f}"
             )
 
         # Increment offset for the next chunk
