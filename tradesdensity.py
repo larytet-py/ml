f3a195c4f8451334373632b31612493731a4235e
arkady <>
2024-05-18 13:45:24 +0300

    trades density timestamps

--- tradesdensity.py
+++ tradesdensity.py
@@ -52,12 +52,12 @@ def process_data_in_chunks(query, chunk_size, interval, min_density):
 
 
         if chunk_trade_density:
-            min_density_record = min(chunk_trade_density, key=lambda x: x[0])
+            #min_density_record = min(chunk_trade_density, key=lambda x: x[0])
             max_density_record = max(chunk_trade_density, key=lambda x: x[0])
-            min_density, min_density_time, min_open_price, min_close_price = min_density_record
+            #min_density, min_density_time, min_open_price, min_close_price = min_density_record
             max_density, max_density_time, max_open_price, max_close_price = max_density_record
-            min_log_density, max_log_density = math.log(min_density),math.log(max_density)
-            chunk_timestamp = chunk_df['timestamp'].iloc[0]
+            max_log_density = math.log(max_density)
+            #chunk_timestamp = chunk_df['timestamp'].iloc[0]
             if max_log_density > 22:
                 # logger.debug(f"{chunk_timestamp}: "
                 #             f"{min_log_density:.2f}@{min_density_time}, price={min_open_price:.0f}/{min_close_price:.0f},"
